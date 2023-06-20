0c602f4f089610ba920bb2bb08dc6d29a476d967
Thomas Luu <luutoan07@gmail.com>
2023-06-20 23:05:33 +0700

    move mongodb to firestore and change method implementing

--- routes/api/crud.js
+++ routes/api/crud.js
@@ -1,77 +1,77 @@
-// Sample file structure
+// // Sample file structure
 
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
+// const express = require('express');
+// const router = express.Router();
+// const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', true);
-mongoose.Promise = global.Promise;
+// mongoose.set('strictQuery', false);
+// mongoose.Promise = global.Promise;
 
-// connect to the database
-mongoose.connect(process.env.MONGODB_URI);
+// // connect to the database
+// mongoose.connect(process.env.MONGODB_URI);
 
-// When successfully connected
-mongoose.connection.on('connected', () => {
-    console.log('Connection to database established successfully');
-});
+// // When successfully connected
+// mongoose.connection.on('connected', () => {
+//     console.log('Connection to database established successfully');
+// });
 
-// If the connection throws an error
-mongoose.connection.on('error', (err) => {
-    console.error(`Error connecting to database: ${err}`);
-});
+// // If the connection throws an error
+// mongoose.connection.on('error', (err) => {
+//     console.error(`Error connecting to database: ${err}`);
+// });
 
-// When the connection is disconnected
-mongoose.connection.on('disconnected', () => {
-    console.log('Database disconnected');
-});
+// // When the connection is disconnected
+// mongoose.connection.on('disconnected', () => {
+//     console.log('Database disconnected');
+// });
 
-/**
- * @method POST
- * @access public
- * @endpoint /api/v1/post
- **/
-router.post('/post', (req, res) => {
-    res.json({
-        message: 'POST API for MERN Boilerplate',
-    });
-});
+// /**
+//  * @method POST
+//  * @access public
+//  * @endpoint /api/v1/post
+//  **/
+// router.post('/post', (req, res) => {
+//     res.json({
+//         message: 'POST API for MERN Boilerplate',
+//     });
+// });
 
-/**
- * @method GET
- * @access public
- * @endpoint /api/v1/get
- **/
-router.get('/get', (req, res) => {
-    res.json({
-        message: 'GET API for MERN Boilerplate',
-        APIs: 'Other Endpoints',
-        create: '/api/v1/post',
-        read: '/api/v1/get',
-        update: '/api/v1/put/<ID>',
-        delete: '/api/v1/delete/<ID>',
-    });
-});
+// /**
+//  * @method GET
+//  * @access public
+//  * @endpoint /api/v1/get
+//  **/
+// router.get('/get', (req, res) => {
+//     res.json({
+//         message: 'GET API for MERN Boilerplate',
+//         APIs: 'Other Endpoints',
+//         create: '/api/v1/post',
+//         read: '/api/v1/get',
+//         update: '/api/v1/put/<ID>',
+//         delete: '/api/v1/delete/<ID>',
+//     });
+// });
 
-/**
- * @method PUT
- * @access public
- * @endpoint /api/v1/put/32323
- **/
-router.put('/put/:id', (req, res) => {
-    res.json({
-        message: `PUT ${req.params.id} API for MERN Boilerplate`,
-    });
-});
+// /**
+//  * @method PUT
+//  * @access public
+//  * @endpoint /api/v1/put/32323
+//  **/
+// router.put('/put/:id', (req, res) => {
+//     res.json({
+//         message: `PUT ${req.params.id} API for MERN Boilerplate`,
+//     });
+// });
 
-/**
- * @method DELETE
- * @access public
- * @endpoint /api/v1/delete/424
- **/
-router.delete('/delete/:id', (req, res) => {
-    res.json({
-        message: `DELETE ${req.params.id} API for MERN Boilerplate`,
-    });
-});
+// /**
+//  * @method DELETE
+//  * @access public
+//  * @endpoint /api/v1/delete/424
+//  **/
+// router.delete('/delete/:id', (req, res) => {
+//     res.json({
+//         message: `DELETE ${req.params.id} API for MERN Boilerplate`,
+//     });
+// });
 
-module.exports = router;
+// module.exports = router;
