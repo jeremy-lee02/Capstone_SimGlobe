e6dbd8a9c2bfc9c3ff2ff59c2fa65651ca9cc786
AlOmEx <omega.ex1234@gmail.com>
2023-07-24 03:58:49 +0700

    Added Lecturer component, added in CSS to hide thumb

--- client/src/components/dashboard/Lecturer.tsx
+++ client/src/components/dashboard/Lecturer.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Lecturer: React.FC = () => {
-  return <h1>Lecturer Component</h1>;
-}
+  const [values, setValues] = useState([
+    { label: 'Real GDP %', value: 50 },
+    { label: 'Unemployment Rate %', value: 30 },
+    { label: 'Inflation Rate %', value: 70 },
+    { label: 'Budget Surplus (Deficit) %', value: 20 },
+    // Add other value-label pairs here
+  ]);
+
+  const handleChangeValue = (index: number, newValue: number) => {
+    setValues((prevValues) => {
+      const updatedValues = [...prevValues];
+      updatedValues[index].value = newValue;
+      return updatedValues;
+    });
+  };
+
+  return (
+    <div className="flex flex-col h-full">
+      <div className="rounded-t-lg p-4">
+        <h1 className="text-3xl pl-2 font-semibold text-white">Room Setting</h1>
+      </div>
+
+      <div className="flex flex-col p-4">
+        {values.map((item, index) => (
+          <div key={index} className="flex items-center m-14">
+            <p className="text-white w-40">{item.label}</p>
+            <input
+              type="range"
+              min={0}
+              max={100}
+              value={item.value}
+              onChange={(e) => handleChangeValue(index, Number(e.target.value))}
+              className="slider h-8 w-full appearance-none"
+              style={{
+                background: `linear-gradient(to right, #f59e0b 0%, #f59e0b ${item.value}%, #4a5568 ${item.value}%, #4a5568 100%)`
+              }}
+            />
+            <p className="text-white ml-4">
+              {item.value} {item.label.endsWith('%') ? '' : 'Max'}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex justify-center mb-4">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Create Room
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Lecturer;
