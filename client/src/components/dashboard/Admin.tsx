09aa9df50b7b0f0b1e014865d9c6f4435c3c1104
AlOmEx <omega.ex1234@gmail.com>
2023-08-21 17:26:11 +0700

    Update Admin Component

--- client/src/components/dashboard/Admin.tsx
+++ client/src/components/dashboard/Admin.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo }from 'react';
+import React, { useState}from 'react';
 import DropboxArrowIcon from '../DropboxArrowIcon';
 const selectedBoxStyle = "bg-[#282C35] bg- border border-gray-300 rounded p-2 w-60 max-w-full text-white text-center";
 const optionBoxStyle = "dropdown-option bg-gray-700 border border-gray-300 rounded p-2 w-60 max-w-full text-white text-center";
@@ -47,81 +47,28 @@ const CustomDropdown: React.FC<{ options: string[], selectedValue: string, onSel
   );
 };
 
-const clusterValues = [
-  {
-    gdp: { min: 0, max: 6 },
-    unemployment: { min: 5, max: 15 },
-    inflation: { '2ndMin': 0, min: 0, max: 12 },
-    budgetSurplus: { min: -5, max: 5 }
-  },
-  {
-    gdp: { min: 2, max: 8 },
-    unemployment: { min: 3, max: 12 },
-    inflation: { '2ndMin': 0, min: 0, max: 15 },
-    budgetSurplus: { min: -5, max: 5 }
-  },
-  {
-    gdp: { min: 1, max: 6 },
-    unemployment: { min: 5, max: 15 },
-    inflation: { '2ndMin': 0, min: 0, max: 18 },
-    budgetSurplus: { min: -5, max: 5 }
-  },
-  {
-    gdp: { min: 0, max: 6 },
-    unemployment: { min: 6, max: 18 },
-    inflation: { '2ndMin': 0, min: 0, max: 12 },
-    budgetSurplus: { min: -10, max: 2 }
-  },
-  {
-    gdp: { min: 0, max: 8 },
-    unemployment: { min: 3, max: 12 },
-    inflation: { '2ndMin': 0, min: 0, max: 12 },
-    budgetSurplus: { min: -8, max: 0 }
-  },
-  {
-    gdp: { min: 2, max: 8 },
-    unemployment: { min: 1, max: 10 },
-    inflation: { '2ndMin': -2, min: 0, max: 6 },
-    budgetSurplus: { min: -10, max: -2 }
-  },
-  {
-    gdp: { min: 0, max: 5 },
-    unemployment: { min: 1, max: 10 },
-    inflation: { '2ndMin': -4, min: 0, max: 5 },
-    budgetSurplus: { min: -6, max: 2 }
-  },
-  {
-    gdp: { min: 0, max: 5 },
-    unemployment: { min: 2, max: 12 },
-    inflation: { '2ndMin': -1, min: 0, max: 7 },
-    budgetSurplus: { min: -10, max: -3 }
-  },
-  {
-    gdp: { min: -1, max: 3 },
-    unemployment: { min: 2, max: 8 },
-    inflation: { '2ndMin': -4, min: 0, max: 5 },
-    budgetSurplus: { min: -10, max: -3 }
-  }
-];
-
 const Administrator: React.FC = () => {
   const [selectedPopulation, setSelectedPopulation] = useState('SMALL');
   const [selectedGDP, setSelectedGDP] = useState('SMALL');
-  const [selectedCluster, setSelectedCluster] = useState(clusterValues[0]);
 
 
   const initialElasticitiesData = [
-    { label: 'Initial Consumption', value: 0 },
-    { label: 'Initial Investment', value: 0 },
-    { label: 'Initial Spending', value: 0 },
-    { label: 'Initial Growth', value: 0 },
-    { label: 'Initial Capital Stock', value: 0 },
-    { label: 'Initial Autonomous Imports', value: 0 },
-    { label: 'Max GDP Score', value: 0 },
-    { label: 'Depreciation', value: 0 },
-    { label: 'Impact of Government Debt on Investment Growth', value: 0 },
-    { label: 'Impact of Real GDP on Unemployment', value: 0 },
-    { label: 'Portion of GDP as Induced Import', value: 0 },
+    { label: 'Perpetual Growth (%)', value: 0 },
+    { label: 'Impact of Inflation on Induced Consumption', value: 0 },
+    { label: 'Impact of Interest Rate (Change) on Induced Consumption', value: 0 },
+    { label: 'Impact of Interest Rate (Level) on Induced Consumption', value: 0 },
+    { label: 'Impact of Interest Rate (Change) on Induced Investment', value: 0 },
+    { label: 'Impact of Interest Rate (Level) on Induced Investment', value: 0 },
+    { label: 'Impact of Interest Rate (Level) on Inflation', value: 0 },
+    { label: 'Impact of Inflation Expectation on Inflation', value: 0 },
+    { label: 'Impact of Supply and Demand change on Inflation', value: 0 },
+    { label: 'Impact of Interest Rate Differential on Capital Flow', value: 0 },
+    { label: 'Autonousmous Import (Billion $USD)', value: 0 },
+    { label: 'Impact of FX Rate on Induced Import', value: 0 },
+    { label: 'Height of Sigmoid', value: 0 },
+    { label: 'Width of Sigmoid', value: 0 },
+    { label: 'Position of Sigmoid', value: 0 },
+    { label: 'Size of Rewards', value: 0 },
   ];
   const [elasticitiesData, setElasticitiesData] = useState(initialElasticitiesData);
 
@@ -136,37 +83,6 @@ const Administrator: React.FC = () => {
     
     console.log("Updated Elasticities Data:", updatedElasticitiesData);
   };
-  useEffect(() => {
-    console.log("Selected GDP:", selectedGDP);
-    console.log("Selected Population:", selectedPopulation);
-  
-    let newSelectedCluster = null;
-  
-    if (selectedGDP === 'SMALL' && selectedPopulation === 'SMALL') {
-      newSelectedCluster = clusterValues[0];
-    } else if (selectedGDP === 'SMALL' && selectedPopulation === 'MEDIUM') {
-      newSelectedCluster = clusterValues[1];
-    } else if (selectedGDP === 'SMALL' && selectedPopulation === 'BIG') {
-      newSelectedCluster = clusterValues[2];
-    } else if (selectedGDP === 'MEDIUM' && selectedPopulation === 'SMALL') {
-      newSelectedCluster = clusterValues[3];
-    } else if (selectedGDP === 'MEDIUM' && selectedPopulation === 'MEDIUM') {
-      newSelectedCluster = clusterValues[4];
-    } else if (selectedGDP === 'MEDIUM' && selectedPopulation === 'BIG') {
-      newSelectedCluster = clusterValues[5];
-    } else if (selectedGDP === 'BIG' && selectedPopulation === 'SMALL') {
-      newSelectedCluster = clusterValues[6];
-    } else if (selectedGDP === 'BIG' && selectedPopulation === 'MEDIUM') {
-      newSelectedCluster = clusterValues[7];
-    } else if (selectedGDP === 'BIG' && selectedPopulation === 'BIG') {
-      newSelectedCluster = clusterValues[8];
-    }
-  
-    if (newSelectedCluster !== null) {
-      setSelectedCluster(newSelectedCluster);
-      console.log("Selected Cluster:", newSelectedCluster);
-    }  
-  }, [selectedGDP, selectedPopulation]);
   const handlePopulationChange = (value: string) => {
     setSelectedPopulation(value);
   };
