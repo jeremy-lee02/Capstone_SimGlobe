7f595ae2c3cd04d31d0b9876864703ef627a925d
Lưu Duy Toàn <ldto@netcompany.com>
2023-08-28 15:25:35 +0700

    setting up websocket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,30 @@
-import EmailVerify from "./components/EmailVerify"
-import GamePlay from "./pages/GamePlay"
-import Home from "./pages/Home"
+
 import { RequireAuth} from "react-auth-kit"
-
-
+import React, { useEffect, useState } from "react";
 import {Routes, Route} from "react-router-dom"
+
 import RoomStudent from "./pages/RoomStudent"
 import RoomHost from "./pages/RoomHost"
 import HomeStudent from "./pages/HomeStudent"
-
+import socketService from "./services/socketService";
+import EmailVerify from "./components/EmailVerify"
+import GamePlay from "./pages/GamePlay"
+import Home from "./pages/Home"
 function App() {
+
+  const connectSocket = async () => {
+    const socket = await socketService
+      .connect("http://localhost:8080")
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+
+  useEffect(() => {
+    connectSocket();
+  }, []);
+
+  
   return (
     <>
       <Routes>
@@ -21,7 +36,6 @@ function App() {
             <GamePlay />
           </RequireAuth>
         } />
-        <Route path= {`/${1}`} element= {<GamePlay />} />
         <Route path= {`/game/:id`} element= {<GamePlay />} />
         <Route path="/room" element={<RoomStudent />} />
         <Route path="/roomhost" element={<RoomHost />} />
