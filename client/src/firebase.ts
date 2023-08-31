855b333b559460b28b865a73a10cb050d8fce414
anhduc2708 <dabrown797@gmail.com>
2023-08-31 16:48:05 +0700

    resolve conflict

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "@firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTNhySCziB1mx3-mMIraWmAsMXIU6tD54",
-  authDomain: "simglobe-e6ce8.firebaseapp.com",
-  projectId: "simglobe-e6ce8",
-  storageBucket: "simglobe-e6ce8.appspot.com",
-  messagingSenderId: "87237194652",
-  appId: "1:87237194652:web:57a512261b96a4b426ce59",
-  measurementId: "G-BQ8Z72YL60"
+  apiKey: "AIzaSyBUT4xYxZ26dtrUobrmRkggoQUkpYeTa4c",
+  authDomain: "simglobe-1.firebaseapp.com",
+  projectId: "simglobe-1",
+  storageBucket: "simglobe-1.appspot.com",
+  messagingSenderId: "938688247662",
+  appId: "1:938688247662:web:61a4b014e699ccbb6d337f",
+  measurementId: "G-84CN4528YW"
 };
   
 
